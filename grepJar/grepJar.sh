00556e2043493448ec15d2e0a72a4ed629457fcd
Brian J Murray <BrianMurray@JHU.edu>
2013-10-30 14:52:47 -0400

    Added arg to sepcify search dir

--- grepJar/grepJar.sh
+++ grepJar/grepJar.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 if [ "$1" == "-h" ]; then
-  echo "Usage: `basename $0` [regular expression]"
+  echo "Usage: `basename $0` regex [search path] "
   exit 0
 fi
-find . -name "*.jar"|while read f
+
+searchPath="$2"
+if [ -z "$2" ]; then
+  searchPath="."
+fi
+
+find $2 -name "*.jar"|while read f
 do
   result=$(unzip -p  $f|strings | grep   $1)
   if [ "$result" ]; then
